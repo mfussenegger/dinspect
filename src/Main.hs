112237fedefe4a11b6ffa871c070b484bf522420
Mathias Fussenegger <f.mathias@zignar.net>
2018-08-21 20:38:48 +0200

    Add output option handling

--- src/Main.hs
+++ src/Main.hs
@@ -3,10 +3,14 @@
 
 module Main where
 
+import           Data.Maybe                             (fromMaybe)
 import           Data.Text                              as T
 import           Graphics.Rendering.Chart.Backend.Cairo
 import           Graphics.Rendering.Chart.Easy
 import           Options.Applicative
+import           System.Directory                       (findExecutable)
+import           System.IO.Temp                         (withSystemTempFile)
+import           System.Process                         (callProcess)
 
 
 data PlotMode = Lines
@@ -15,8 +19,8 @@ data PlotMode = Lines
 
 data PlotOptions = PlotOptions
   { mode :: PlotMode
-  , verbose :: Bool
   , title :: T.Text
+  , output :: Maybe FilePath
   }
   deriving (Show)
 
@@ -28,22 +32,23 @@ plotOptions =
     ( long "mode"
     <> metavar "MODE"
     <> help "Type of plot to use" )
-  <*> switch
-    ( long "verbose"
-    <> help "Enable verbose output" )
   <*> strOption
     ( long "title"
     <> metavar "TITLE"
     <> value ""
     <> help "Title of the plot" )
+  <*> option auto
+    ( long "output"
+    <> value Nothing
+    <> help "Output file" )
 
 
 commands = hsubparser
   ( command "plot" (info plotOptions (progDesc "Plot data")) )
 
 
-doPlot :: PlotMode -> T.Text -> IO ()
-doPlot mode title = toFile def "dummy.png" $ do
+plotToFile :: PlotMode -> T.Text -> FilePath -> IO ()
+plotToFile mode title output = toFile def output $ do
   layout_title .= T.unpack title
   plot (line "1" [values])
   plot (line "2" [fmap (\(x, y) -> (x, y + 10)) values])
@@ -52,11 +57,23 @@ doPlot mode title = toFile def "dummy.png" $ do
     values = [(1, 1), (2, 2), (3, 3), (4, 4)]
 
 
+doPlot :: PlotMode -> T.Text -> Maybe FilePath -> IO ()
+doPlot mode title mOutput =
+  case mOutput of
+    (Just output) -> plotToFile mode title output
+    _             -> withSystemTempFile "inspect-plot" plotThenOpen
+  where
+    plotThenOpen tmpFile tmpHandle = do
+      plotToFile mode title tmpFile
+      openCmd <- fromMaybe "xdg-open" <$> findExecutable "open"
+      callProcess openCmd [tmpFile]
+
+
 main :: IO ()
 main = do
   command <- execParser opts
   case command of
-    PlotOptions{..} -> doPlot mode title
+    PlotOptions{..} -> doPlot mode title output
   where
     opts = info (commands <**> helper)
       ( fullDesc <> progDesc "Inspect data" )
